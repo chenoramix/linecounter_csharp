76e25da7d3fa110b9a50a35f113602dc27055df9
Krzysztof Mazur <m.krzysztof@gmail.com>
2023-01-07 16:24:36 +0100

    First commit

---
+++ linecounter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace linecounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
