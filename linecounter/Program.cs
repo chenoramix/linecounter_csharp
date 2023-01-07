d4278a058100c5c1465dbbd3769f2b857f1ee3ed
Krzysztof Mazur <m.krzysztof@gmail.com>
2023-01-07 19:15:56 +0100

    Added get files method to obtain all path matching the pattern filename,

--- linecounter/Program.cs
+++ linecounter/Program.cs
@@ -1,13 +1,31 @@
-﻿namespace linecounter
+﻿using System.IO;
+
+
+namespace linecounter
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            if(args.Length < 2)
+            if(args.Length != 2)
             {
                 Console.WriteLine("Usage:");
-                Console.WriteLine("linecounter.exe c:\\path *.cs");
+                Console.WriteLine(@"linecounter.exe c:\path *.cs");
+                return;
+            }
+
+            if(!Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Directory doesn't exist, sorry.");
+                return;
+            }
+
+            // GetFiles(path, filePatter, searchOption);
+            string[] files = Directory.GetFiles(args[0], args[1], SearchOption.AllDirectories);
+            if(files.Length == 0)
+            {
+                Console.WriteLine("Couldn't find any files matching pattern:");
+                Console.WriteLine(args[1]);
                 return;
             }
 
