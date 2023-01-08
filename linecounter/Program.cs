eccdadb7a3b2e69f8b2a4017e96be111a7a67a84
Krzysztof Mazur <m.krzysztof@gmail.com>
2023-01-08 18:01:17 +0100

    Added lines counting.

--- linecounter/Program.cs
+++ linecounter/Program.cs
@@ -1,10 +1,32 @@
 ﻿using System.IO;
-
+using System.IO.Enumeration;
 
 namespace linecounter
 {
     internal class Program
     {
+        /*
+        static void processDirectories(string sDir, List<string> paths, string pattern)
+        {
+            try
+            {
+                foreach(string dir in Directory.GetDirectories(sDir))
+                {
+                    foreach(string file in Directory.GetFiles(dir))
+                    {
+                        bool isMatch = FileSystemName.MatchesSimpleExpression(pattern, file);
+                        if(isMatch)
+                        {
+                            paths.Add(file);
+                        }
+                    }
+                    processDirectories(dir, paths, pattern);
+                }
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }*/
         static void Main(string[] args)
         {
             if(args.Length != 2)
@@ -20,8 +42,17 @@ namespace linecounter
                 return;
             }
 
-            // GetFiles(path, filePatter, searchOption);
-            string[] files = Directory.GetFiles(args[0], args[1], SearchOption.AllDirectories);
+            string[] files = Array.Empty<string>();
+            try
+            {
+                files = Directory.GetFiles(args[0], args[1], SearchOption.AllDirectories);
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Exiting...");
+                return;
+            }
+
             if(files.Length == 0)
             {
                 Console.WriteLine("Couldn't find any files matching pattern:");
@@ -29,7 +60,18 @@ namespace linecounter
                 return;
             }
 
-            Console.ReadLine();
+            decimal lineCounter = 0;
+
+            foreach(string file in files)
+            {
+                foreach(string line in File.ReadLines(file))
+                {
+                    lineCounter++;
+                }
+            }
+
+            Console.WriteLine($"Counted lines: {lineCounter}");
+            Console.WriteLine($"In {files.Length} files");
         }
     }
 }
